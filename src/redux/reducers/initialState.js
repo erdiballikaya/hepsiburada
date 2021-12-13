81b33658f56e258dd6192b2be20e79c0ecff60cb
erdiballikaya <79500217+erdiballikaya@users.noreply.github.com>
2021-12-13 19:19:40 +0300

    fixese

--- src/redux/reducers/initialState.js
+++ src/redux/reducers/initialState.js
@@ -4,6 +4,6 @@ export default {
     tagColors: [],
     tagBrands: [],
     searchString: "Iphone",
-    basket: localStorage.getItem("basketCount") !== 0 ? localStorage.getItem("basketCount") : 0,
-    productsInBasket:localStorage.getItem("productsInBasket") !== 0 ? JSON.parse(localStorage.getItem("productsInBasket")) : [],
+    basket: localStorage.getItem("basketCount") === null ?  0 : localStorage.getItem("basketCount") ,
+    productsInBasket: JSON.parse(localStorage.getItem("productsInBasket")) !== null ? JSON.parse(localStorage.getItem("productsInBasket")) : [],
 }
